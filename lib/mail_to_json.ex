3e8e9caff807571b98d1c57ced7e81a2ba459f2a
Akash Manohar J <akash@akash.im>
2014-10-09 15:09:06 +0530

    Remove comments

--- lib/mail_to_json.ex
+++ lib/mail_to_json.ex
@@ -1,18 +1,13 @@
 defmodule MailToJson do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
     children = [
-      # Define workers and child supervisors to be supervised
-      # worker(MailToJson.Worker, [arg1, arg2, arg3])
+      worker(MailToJson.SmtpHandler, [], restart: :transient)
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: MailToJson.Supervisor]
     Supervisor.start_link(children, opts)
   end
