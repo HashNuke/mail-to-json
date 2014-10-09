6c4a409f1e8582bc7e11b92688954fa129b43fc5
Akash Manohar J <akash@akash.im>
2014-10-09 15:06:06 +0530

    Add dependencies to mix.exs and lock them

--- mix.exs
+++ mix.exs
@@ -8,24 +8,18 @@ defmodule MailToJson.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
+
   def application do
     [applications: [:logger],
      mod: {MailToJson, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
+
   defp deps do
-    []
+    [
+      {:gen_smtp,  github: "Vagabond/gen_smtp"},
+      {:poison,    github: "devinus/poison"},
+      {:httpoison, github: "edgurgel/httpoison"}
+    ]
   end
 end
