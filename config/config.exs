e0ec58c92f18c368091cf74877037b69e6261219
Akash Manohar J <akash@akash.im>
2014-10-14 22:27:32 +0530

    Changes to notes

--- config/config.exs
+++ config/config.exs
@@ -1,4 +1,7 @@
 use Mix.Config
 
-config :mail_to_json, :webhook_url, (System.get_env("M2J_WEBHOOK_URL") || "")
-config :mail_to_json, :smtp_port,   (System.get_env("M2J_SMTP_PORT")   || 2525)
+# This is where you want the JSON of the mail to be posted to
+config :mail_to_json, :webhook_url, System.get_env("M2J_WEBHOOK_URL")
+
+# The SMTP port to which we want our application to listen to
+config :mail_to_json, :smtp_port,   (System.get_env("M2J_SMTP_PORT") || 2525)
